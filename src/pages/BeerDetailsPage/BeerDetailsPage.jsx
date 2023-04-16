40602b5656271aac1fc3c7c05997083290cffd60
Feras Haj Saleh <>
2023-04-16 10:58:33 +0200

    adding two elements to the page just to test the source tree

--- src/pages/BeerDetailsPage/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage/BeerDetailsPage.jsx
@@ -1,44 +1,46 @@
-import React from 'react';
-import './BeerDetailsPage.css';
-import { useParams } from 'react-router-dom';
-import Footer from '../../components/Footer/Footer';
-import arrow from '../../assets/images/left-arrow.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./BeerDetailsPage.css";
+import { useParams } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import arrow from "../../assets/images/left-arrow.png";
+import { Link } from "react-router-dom";
 
 const BeerDetailsPage = ({ data }) => {
-    const params = useParams();
-    const beer = data.filter((element) => element._id === params.id);
+	const params = useParams();
+	const beer = data.filter((element) => element._id === params.id);
 
-    return (
-        <div>
-            <section className="details-section">
-                <div className="beer-details">
-                    <div className="img-container">
-                        <img src={beer[0].image_url} alt="beer bottle" />
-                    </div>
-                    <h3>{beer[0].name}</h3>
-                    <h4>{beer[0].tagline}</h4>
-                    <div className="infos">
-                        <div>
-                            <span>First brewed:</span>
-                            <span>{beer[0].first_brewed}</span>
-                        </div>
-                        <div>
-                            <span>Attenuation level:</span>
-                            <span>{beer[0].attenuation_level}</span>
-                        </div>
-                    </div>
-                    <p className="descriptions">{beer[0].description}</p>
-                    <Link to="/list">
-                        <button className="arrow">
-                            <img src={arrow} alt="left-arrow" />
-                        </button>
-                    </Link>
-                </div>
-            </section>
-            <Footer />
-        </div>
-    );
+	return (
+		<div>
+			<section className="details-section">
+				<div className="beer-details">
+					<h1>Hallo World</h1>
+					<p>We have alot of Beer sorts</p>
+					<div className="img-container">
+						<img src={beer[0].image_url} alt="beer bottle" />
+					</div>
+					<h3>{beer[0].name}</h3>
+					<h4>{beer[0].tagline}</h4>
+					<div className="infos">
+						<div>
+							<span>First brewed:</span>
+							<span>{beer[0].first_brewed}</span>
+						</div>
+						<div>
+							<span>Attenuation level:</span>
+							<span>{beer[0].attenuation_level}</span>
+						</div>
+					</div>
+					<p className="descriptions">{beer[0].description}</p>
+					<Link to="/list">
+						<button className="arrow">
+							<img src={arrow} alt="left-arrow" />
+						</button>
+					</Link>
+				</div>
+			</section>
+			<Footer />
+		</div>
+	);
 };
 
 export default BeerDetailsPage;
