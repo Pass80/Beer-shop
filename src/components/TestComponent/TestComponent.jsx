6263e9b043bbcc45466d8f43765a30e0d3b77a3d
Feras Haj Saleh <>
2023-04-22 00:09:51 +0200

    adding some unordered list to the beer3 branch

---
+++ src/components/TestComponent/TestComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return (
+        <div>
+            <h1></h1>
+            <div className="fre">
+                <div className="column">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default TestComponent;
